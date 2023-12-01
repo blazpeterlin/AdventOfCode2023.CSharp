922b130932f7ba0cf2cc232a2deb08d728b8cad5
Blaž Peterlin <blasius.peterlin@gmail.com>
2023-12-01 05:57:24 +0100

    Initial commit

---
+++ AdventOfCode2023.CSharp/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aoc2023.ActiveDay;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input =
+                """""
+
+                """"";
+
+            var sln = new Solution();
+            sln.Solve1(input);
+            sln.Solve2(input);
+        }
+    }
+}
---
+++ AdventOfCode2023.CSharp/ConsoleApp1/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Environment;
+
+namespace Aoc2023.ActiveDay
+{
+    internal class Solution
+    {
+        public int Solve1(string input)
+        {
+            //string s = NewLine;
+            return 0;
+        }
+
+        public int Solve2(string input)
+        {
+            return 0;
+        }
+    }
+}
