bfe285ab4ee9fc533da8d15919d361c17a8c280d
Blaž Peterlin <blasius.peterlin@gmail.com>
2023-12-02 06:13:02 +0100

    Day 2 (initial)

--- AdventOfCode2023.CSharp/ConsoleApp1/Solution.cs
+++ AdventOfCode2023.CSharp/ConsoleApp1/Solution.cs
@@ -12,14 +12,92 @@ namespace Aoc2023.ActiveDay
 {
     internal class Solution
     {
+
+
         public int Solve1(string input)
         {
-            return 0;
+            var lns = Regex.Split(input, NewLine)
+                .Where(ln => ln != "")
+                .Select(ln => Regex.Split(ln, @"(?:\:\s|;\s|,\s)").ToList())
+                .ToList();
+
+
+            var parsed = lns.Select(tkns =>
+            {
+                Dictionary<string, int> req = new();
+                int id = int.Parse(tkns[0].Split(new[] { ' ', ':' })[1]);
+
+                var coloured = tkns.Skip(1).ToList();
+                foreach(var clrTkn in coloured.Select(c => c.Split(" ")))
+                {
+                    int num = int.Parse(clrTkn[0]);
+                    string ty = clrTkn[1];
+                    if (!req.ContainsKey(ty))
+                    {
+                        req[ty] = num;
+                    }
+                    else
+                    {
+                        req[ty] = Math.Max(req[ty], num);
+                    }
+                    
+                }
+
+                return (id, req);
+            });
+
+            var res = parsed.Where(prs =>
+            {
+                var p = prs.req;
+                return p["red"] <= 12
+                    && p["green"] <= 13
+                    && p["blue"] <= 14;
+            }).Select(prs => prs.id).Sum();
+
+            // not 369
+            // not 1681
+            return res;
         }
 
         public int Solve2(string input)
         {
-            return 0;
+            var lns = Regex.Split(input, NewLine)
+                .Where(ln => ln != "")
+                .Select(ln => Regex.Split(ln, @"(?:\:\s|;\s|,\s)").ToList())
+                .ToList();
+
+
+            var parsed = lns.Select(tkns =>
+            {
+                Dictionary<string, int> req = new();
+                int id = int.Parse(tkns[0].Split(new[] { ' ', ':' })[1]);
+
+                var coloured = tkns.Skip(1).ToList();
+                foreach (var clrTkn in coloured.Select(c => c.Split(" ")))
+                {
+                    int num = int.Parse(clrTkn[0]);
+                    string ty = clrTkn[1];
+                    if (!req.ContainsKey(ty))
+                    {
+                        req[ty] = num;
+                    }
+                    else
+                    {
+                        req[ty] = Math.Max(req[ty], num);
+                    }
+
+                }
+
+                return (id, req);
+            });
+
+            var res = parsed.Select(prs =>
+            {
+                var p = prs.req;
+                return p["red"] * p["green"] * p["blue"];
+            }).Sum();
+
+            return res;
         }
     }
 }
